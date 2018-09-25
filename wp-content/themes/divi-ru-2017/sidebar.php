49e160bc0d6f768689ba56f9fb9033d986fadb4f
John Bowen <john.bowen@rutgers.edu>
2018-09-25 11:21:21 -0400

    remove divi-ru-2017 child theme

--- wp-content/themes/divi-ru-2017/sidebar.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ( is_single() || is_page() ) && 'et_full_width_page' === get_post_meta( get_the_ID(), '_et_pb_page_layout', true ) )
-	return false;
-
-if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-	<div id="sidebar" class="et_pb_widget_area_right">
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	</div> <!-- end #sidebar -->
-<?php endif; ?>
